53504fab42ee63e1cdc3aa92bc46df4b55e8b97d
Rohit Anantha <anantha.rohit.11@gmail.com>
2021-10-28 20:46:07 -0500

    idk some fixes or smt

--- Scripts/Tester.java
+++ Scripts/Tester.java
@@ -1,11 +1,15 @@
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.HashMap;
 
 public class Tester {
     public static void main(String[] args) throws MalformedURLException, IOException {
         MenuParser mParser = new MenuParser();
 
         DiningURL dURL = new DiningURL("Kins Dining", "Lunch");
-        System.out.println(mParser.getNutritionalValuesForAllFoods(dURL.getCompleteURL()).keySet());
+        HashMap<String, HashMap<String, String>> hash =
+                mParser.getNutritionalValuesForAllFoods(dURL.getCompleteURL());
+        // System.out.println(hash);
+        System.out.println(hash.get("Breadstick").get("Calories"));
     }
 }
